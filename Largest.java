2c7697b1491cb7a3937af2d5cd8eaa7581732268
roshi <srivastavaroshi00@gmail.com>
2025-01-17 15:34:51 +0530

    largest of 2 numbers

---
+++ Largest.java
@@ -0,0 +1,8 @@
+public class Largest {
+    public static void main(String args[]) {
+        int a = 5;
+            int b = 8;
+            int c = Math.max(a,b);
+            System.out.println(c);
+      }
+}
